5cc2935717ebd3604c63d92220417de2fbb4350c
Fred20146 <114985646+Fred20146@users.noreply.github.com>
2026-01-15 16:17:20 +0100

    tremblement + destruction

--- Assets/Cible.cs
+++ Assets/Cible.cs
@@ -5,9 +5,47 @@ public class Cible : MonoBehaviour
     [Header("Vie")]
     public int life = 10;
 
+    [Header("Tremblement")]
+    public float shakeIntensity = 0.05f;
+    public float shakeDuration = 0.2f;
+
+    private Vector3 basePosition;
+    private float shakeTimer;
+
+    void Start()
+    {
+        basePosition = transform.localPosition;
+    }
+
+    void Update()
+    {
+        if (shakeTimer > 0)
+        {
+            shakeTimer -= Time.deltaTime;
+            transform.localPosition = basePosition + Random.insideUnitSphere * shakeIntensity;
+        }
+        else
+        {
+            transform.localPosition = basePosition;
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        print("Collision");
+        if (collision.gameObject.CompareTag("Projectile"))
+        {
+            Damage(1);
+        }
+    }
+
+    void Damage(int amount)
+    {
+        life -= amount;
+        shakeTimer = shakeDuration;
+
+        if (life <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
-
